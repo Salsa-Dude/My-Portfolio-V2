5b8cd14aab68da9b6f194fc14da56525aac28b80
Joseph Arias <arias.joseph.a@gmail.com>
2021-08-16 04:42:15 -0400

    implemented bootstrap require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require jquery-ui
 //= require rails-ujs
 //= require activestorage
+//= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
+//=require custom
+
