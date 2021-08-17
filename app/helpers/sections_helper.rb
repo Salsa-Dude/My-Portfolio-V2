2b4e44d64899065c01d2e596a64053fb2a5cd9a3
Joseph Arias <arias.joseph.a@gmail.com>
2021-08-17 19:08:10 -0400

    implemented helper method experience_items

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,2 +1,21 @@
 module SectionsHelper
+
+  def experience_items
+    [
+      {
+        company: "Nfinity LLC",
+        url: "https://nfinityllc.com/modern-application-development/",
+        time: "June 2020 - Present",
+        position: "Junior Web Developer"
+      },
+      {
+        company: "Flatiron School",
+        url: "https://flatironschool.com/campus-and-online-coding-bootcamp/",
+        time: "April 2019 - May 2020",
+        position: "Software Engineering Teaching Assistant"
+      }
+    ]
+  end
+
+
 end
